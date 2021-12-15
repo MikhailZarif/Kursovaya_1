fb996b83cc814f58ed7b8730b0bf71e69d14e2ed
MikhailZarif <mishka.suren@gmail.com>
2021-12-15 15:07:59 +0300

    working in and out streams, can analyze absolute stability

--- demo/main.cpp
+++ demo/main.cpp
@@ -4,76 +4,76 @@
 #include <boost/filesystem.hpp>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <header.hpp>
 #include <stdexcept>
 #include <fstream>
+#include "header.hpp"
+#include <cstdlib>
+
 
 using json = nlohmann::json;
 
 int main() {
     std::cout << "Enter Your data" << std::endl;
     // Пользователь имеет три возможности ввести данные:
-    //  - через консоль, тогда пишет строку "open text"
+    //  - через консоль, тогда пишет строку "open_text"
     //  - из файла txt, тогда вводит полный путь к файлу
     //  - из файла json, тогда пишет полный путь к файлу
     std::string in, X, Y;
     std::vector<int> K;
-    char **function_table = nullptr;
-    // X-открытый текст Y-шифртекст K-множество ключей function_table-функция шифрования, представленная в виде таблицы
+    std::vector<std::string> func;
+    // X-открытый текст Y-шифртекст K-множество ключей func - вектор функций шифрования
     // В конце каждой введённой новой переменной в консоли и в txt файле идёт строка с ключевым словом EOT, EOC, EOK (End Of Text/Cipher/Key)
     std::cin >> in;
     if (in == "open_text") {
+        // Открытый текст
         std::cout << "Enter Your text, don't forget EOT on the next line after text" << std::endl;
-        std::cin >> in;
+        getline(std::cin, in);
         while (in != "EOT") {
             X += in;
-            std::cin >> in;
+            getline(std::cin, in);
         }
         if (X.empty())
             throw std::logic_error{"Text should not be empty"};
+        // Шифртекст
         std::cout << "Enter Your encrypted text, don't forget EOC on the next line after encrypted text" << std::endl;
-        std::cin >> in;
+        getline(std::cin, in);
         while (in != "EOC") {
             Y += in;
-            std::cin >> in;
+            getline(std::cin, in);
         }
         if (Y.empty())
             throw std::logic_error{"Cipher should not be empty"};
+        // Ключи
         std::cout
                 << "Enter Your key array, put ' ' or ',' between keys, don't forget EOK on the next line after last key"
                 << std::endl;
-        std::cin >> in;
+        getline(std::cin, in);
         while (in != "EOK") {
             std::string key;
-            for (char i: in) {
-                if ((i != ' ') && (i != ',')) {
-                    key += i;
-                } else {
+            for (size_t i = 0; i < in.size(); ++i) {
+                if ((in[i] != ' ') && (in[i] != ',')) {
+                    key += in[i];
+                }
+                if ((in[i] == ' ') || (in[i] == ',') || (i == in.size() - 1)) {
                     K.push_back(std::stoi(key));
                     key = "";
                 }
             }
-            std::cin >> in;
+            getline(std::cin, in);
         }
         if (K.empty())
             throw std::logic_error{"Key array should not be empty"};
-        std::cout << "Enter Your encryption function mode, now only available 'table'" << std::endl;
-        std::cin >> in;
-        if (in == "table") {
-            delete function_table;
-            std::string cut;
-            for (char &i: X)
-                if ((std::find(cut.begin(), cut.end(), i) == cut.end()) || (cut.empty()))
-                    cut += i;
-            function_table = new char *[cut.length()];
-            for (size_t i = 0; i < cut.length(); ++i) {
-                function_table[i] = new char[K.size()];
-                std::cin >> in;
-                for (size_t j = 0; j < K.size(); ++j)
-                    function_table[i][j] = in[j];
-            }
+        // Функции
+        std::cout
+                << "Enter Your functions, one function - one line, don't forget EOF on the next line after last function"
+                << std::endl;
+        getline(std::cin, in);
+        while (in != "EOF") {
+            func.push_back(in);
+            getline(std::cin, in);
         }
+        if (func.empty())
+            throw std::logic_error{"Function array should not be empty"};
     } else {
         boost::filesystem::path p{in};
         if (p.empty())
@@ -85,6 +85,7 @@ int main() {
             is.open(in);
             if (is.peek() == std::ifstream::traits_type::eof())
                 throw std::logic_error{"File must not be empty"};
+            // Открытый текст
             getline(is, in);
             while (in != "EOT") {
                 X += in;
@@ -92,8 +93,7 @@ int main() {
             }
             if (X.empty())
                 throw std::logic_error{"Text should not be empty"};
-            getline(is, in);
-
+            // Шифртекст
             getline(is, in);
             while (in != "EOC") {
                 Y += in;
@@ -101,15 +101,15 @@ int main() {
             }
             if (Y.empty())
                 throw std::logic_error{"Cipher should not be empty"};
-            getline(is, in);
-
+            // Ключи
             getline(is, in);
             while (in != "EOK") {
                 std::string key;
-                for (char i: in) {
-                    if ((i != ' ') && (i != ',')) {
-                        key += i;
-                    } else {
+                for (size_t i = 0; i < in.size(); ++i) {
+                    if ((in[i] != ' ') && (in[i] != ',')) {
+                        key += in[i];
+                    }
+                    if ((in[i] == ' ') || (in[i] == ',') || (i == in.size() - 1)) {
                         K.push_back(std::stoi(key));
                         key = "";
                     }
@@ -118,19 +118,14 @@ int main() {
             }
             if (K.empty())
                 throw std::logic_error{"Key array should not be empty"};
-
-            delete function_table;
-            std::string cut;
-            for (char &i: X)
-                if ((std::find(cut.begin(), cut.end(), i) == cut.end()) || (cut.empty()))
-                    cut += i;
-            function_table = new char *[cut.length()];
-            for (size_t i = 0; i < cut.length(); ++i) {
-                function_table[i] = new char[K.size()];
+            // Функции
+            getline(is, in);
+            while (in != "EOF") {
+                func.push_back(in);
                 getline(is, in);
-                for (size_t j = 0; j < K.size(); ++j)
-                    function_table[i][j] = in[j];
             }
+            if (func.empty())
+                throw std::logic_error{"Function array should not be empty"};
         }
         if (p.extension() == ".json") {
             json data;
@@ -139,53 +134,39 @@ int main() {
             if (data.empty())
                 throw std::logic_error{"File must not be empty"};
 
+            if (data.at("text").empty())
+                throw std::logic_error{"Text must not be empty"};
             if (!data.at("text").is_string())
                 throw std::logic_error{"Text must be string"};
             X = data.at("text").get<std::string>();
-            if (X.empty())
-                throw std::logic_error{"Text must not be empty"};
 
+            if (data.at("cipher").empty())
+                throw std::logic_error{"Cipher must not be empty"};
             if (!data.at("cipher").is_string())
                 throw std::logic_error{"Cipher must be string"};
             Y = data.at("cipher").get<std::string>();
-            if (Y.empty())
-                throw std::logic_error{"Cipher must not be empty"};
 
+            if (data.at("key").empty())
+                throw std::logic_error{"Key must not be empty"};
             if (!data.at("key").is_array())
                 throw std::logic_error{"Key must be array"};
             K = data.at("key").get<std::vector<int>>();
-            if (K.empty())
-                throw std::logic_error{"Key must not be empty"};
 
-            if (!data.at("table").empty()) {
-                if (!data.at("table").is_array())
-                    throw std::logic_error{"Function table must be array of array of char"};
-                delete function_table;
-                std::string cut;
-                for (char &i: X)
-                    if ((std::find(cut.begin(), cut.end(), i) == cut.end()) || (cut.empty()))
-                        cut += i;
-                function_table = new char *[cut.length()];
-                for (size_t i = 0; i < cut.length(); ++i) {
-                    function_table[i] = new char[K.size()];
-                    std::vector<char> line;
-                    line = data.at("table").get<std::vector<char>>();
-                    for (size_t j = 0; j < K.size(); ++j)
-                        function_table[i][j] = line[j];
-                }
-            }
+            if (data.at("functions").empty())
+                throw std::logic_error{"Functions array must not be empty"};
+            if (!data.at("functions").is_array())
+                throw std::logic_error{"Function table must be array of array of char"};
+            func = data.at("functions").get<std::vector<std::string>>();
         }
     }
-    // Проверка
-    std::cout << X << std::endl;
-    std::cout << Y << std::endl;
-    for (int i: K)
-        std::cout << i << ' ';
-    std::cout << std::endl;
-    for (size_t i = 0; i < powerX(X); ++i) {
-        for (size_t j = 0; j < K.size(); ++j)
-            std::cout << function_table[i][j] << ' ';
-        std::cout << std::endl;
-    }
     // /home/enenra/kursovaya/Kursovaya_1/data.txt
+    std::ofstream out("/home/enenra/kursovaya/Kursovaya_1/Report.txt", std::ios::app);
+    out << "Report on Your cryptoalgorythm" << '\n';
+    out << "Here are the checks that Your algorithm has passed" << '\n';
+    out << " - Absolute stability: ";
+    if (absolute_stability(X, K))
+        out << "true" << '\n';
+    else
+        out << "false" << '\n';
+    out.close();
 }
--- include/header.hpp
+++ include/header.hpp
@@ -10,15 +10,33 @@
 
 using json = nlohmann::json;
 
+class function {
+private:
+    std::string func;
+public:
+    function() = default;
+
+    function(std::string &f) {
+        func = f;
+    }
+};
+
 size_t powerX(std::string &X) {
     size_t res = 0;
     std::string cut;
-    for (char & i : X)
-        if ((std::find(cut.begin(), cut.end(), i) == cut.end())||(cut.empty())) {
+    for (char &i: X)
+        if ((std::find(cut.begin(), cut.end(), i) == cut.end()) || (cut.empty())) {
             cut += i;
             ++res;
         }
     return res;
+}
+
+bool absolute_stability (std::string& text, std::vector<int>& keys)
+{
+    if (powerX(text)<=keys.size())
+        return true;
+    return false;
 }
 
 #endif //KURSOVAYA_1_HEADER_HPP
